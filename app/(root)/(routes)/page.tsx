45a91334e326d100a1c7da48d141f7e97c83c6b3
CharulRathore <rathore0ch@gmail.com>
2024-10-18 23:15:05 -0700

    Category Filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,18 @@
+import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
 
 const RootPage = () => {
+    const categories = [
+        { id: "1", name: "Famous People" },
+        { id: "2", name: "Movies & TV" },
+        { id: "3", name: "Musicians" },
+        { id: "4", name: "Games" },
+        { id: "5", name: "Animals" },
+    ];
     return ( 
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data={categories}/>
         </div>
     );
 }
