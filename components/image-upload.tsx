dc3050c3c44fdb62b37b75584ddb0e419a7817fc
Charul Rathore <rathore0ch@gmail.com>
2024-10-20 01:35:07 -0700

    Form started

---
+++ components/image-upload.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+
+interface ImageUploadProps {
+    value: string;
+    onChange: (src: string) => void;
+    disabled?: boolean;
+}
+
+export const ImageUpload = ({
+    value,
+    onChange,
+    disabled
+}: ImageUploadProps) => {
+    // Hydration Errors.
+}
