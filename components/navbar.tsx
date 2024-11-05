4b1bd1bc891b78ce7dc6ce339b6cb6bec2227ccc
Charul Rathore <rathore0ch@gmail.com>
2024-11-04 23:24:13 -0800

    navbar color change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,32 +10,35 @@ import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 import { MobileSidebar } from "@/components/mobile-sidebar";
 
-
 const font = Poppins({
-    weight: "600",
-    subsets: ["latin"]
+  weight: "600",
+  subsets: ["latin"]
 });
 
 export const Navbar = () => {
-    return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
-            <div className="flex items-center">
-                <MobileSidebar />
-                <Link href="/">
-                <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className
-                )}>
-                    ai.teaches.ai
-                </h1>
-                </Link>
-            </div>
-            <div className="flex items-center gap-x-3">
-                <Button  variant="premium" size="sm">
-                    Upgrade
-                    <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
-                </Button>
-                <ModeToggle />
-                <UserButton afterSwitchSessionUrl="/" />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-6 shadow-lg border-b border-primary/10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white h-16">
+      <div className="flex items-center">
+        <MobileSidebar />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-extrabold tracking-tight text-white",
+              font.className
+            )}
+          >
+            BrightMind.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-4">
+        <Button variant="premium" size="default" className="bg-black text-white border-0 hover:bg-gradient-to-r hover:from-sky-500 hover:via-blue-500 hover:to-cyan-500 duration-200  transition-all ease-in-out">   
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white ml-1" />
+        </Button>
+        <ModeToggle />
+        <UserButton afterSwitchSessionUrl="/" />
+      </div>
+    </div>
+  );
+};
